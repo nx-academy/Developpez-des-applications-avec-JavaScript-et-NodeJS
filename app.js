a613cbf428b72bc0004100bf14e5913340255a6a
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-02-24 15:03:13 +0100

    Updating the console.log

--- app.js
+++ app.js
@@ -1,36 +1,5 @@
-const readline = require('readline')
-
-const inquierer = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-function generateRandomNumber() {
-  const maxNumber = 10
-  return Math.floor(Math.random() * maxNumber) + 1
-}
-
-const randomNumber = generateRandomNumber()
-let numberOfAttempts = 0
-
 function main() {
-  numberOfAttempts++
-
-  inquierer.question("Pick a number between 1 and 10: ", function(input) {
-    if (Number(input) === randomNumber) {
-      console.log("Well done")
-      console.log(`Your number of attempts was ${numberOfAttempts}`)
-      process.exit()
-    } else if (Number(input) > randomNumber) {
-      console.log("Too high")
-    } else if (Number(input) < randomNumber) {
-      console.log("Too low")
-    } else {
-      console.log("Invalid Input")
-    }
-
-    main() //Calling this function again to continue the game
-  })
+  console.log("Bonjour de la branche du chapitre 3 de la partie 1")
 }
 
 main()
