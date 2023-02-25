74c89065fb57168dc98b3535174102cbb4e85841
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-02-25 15:27:23 +0100

    Adding sample types

---
+++ type.js
@@ -0,0 +1,23 @@
+
+// Je déclare une constante
+const myFirstName = "Thomas"
+
+// Je déclare une autre constante
+const myLastName = "Dimnet"
+
+
+// J'initialise une variable
+let myAge = 34
+
+// Je modifie la valeur de myAge
+myAge = 35
+
+// Je modifie une dernière la valeur de myAge
+myAge = 36
+
+
+// J'initialise une autre variable
+let myLocation = "Paris"
+
+// Je change le type et la valeur de myLocation
+myLocation = 75020
