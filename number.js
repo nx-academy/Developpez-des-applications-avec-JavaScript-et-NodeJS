9e5f6826b159f16dc0470e8d161dd084922a5c61
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-02-27 10:34:50 +0100

    Adding a file dedicated to numbers

---
+++ number.js
@@ -0,0 +1,20 @@
+// J'initialise une valeur
+let numberOfPeople = 60
+
+console.log("L'état initial de numberOfPeople est", numberOfPeople)
+
+// Je l'aditionne (incrémente) de 20
+numberOfPeople += 20
+console.log("numberOfPeople incrémenté de 20 vaut", numberOfPeople)
+
+// Je le soustraits (décrémente) de 10
+numberOfPeople -= 10
+console.log("numberOfPeople décrémenté de 10 vaut", numberOfPeople)
+
+// Je multiplie numberOfPeople par 5
+numberOfPeople *= 5
+console.log("numberOfPeople multiplié par 5 vaut", numberOfPeople)
+
+// Je divise numberOfPeople par 2
+numberOfPeople /= 2
+console.log("numberOfPeople divisé par 2 vaut", numberOfPeople)
