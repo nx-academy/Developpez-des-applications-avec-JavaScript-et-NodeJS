9309af5b55fe949964f99dc19bb28e940f08efb1
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-02-27 14:18:00 +0100

    Adding the solution

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-function main() {
-    console.log("Hello, World!")
-}
+// Première User Story
+const maximumNumberOfTries = 10
+console.log(`le nombre maximum d'essais est de ${maximumNumberOfTries}`)
 
-main()
+
+// Deuxième User Story
+let numberOfAttempts = 0
+numberOfAttempts += 2
+console.log(`le nombre de tentatives réalisées est de ${numberOfAttempts}`)
+
+numberOfAttempts += 1
+console.log(`le nombre de tentatives réalisées est de ${numberOfAttempts}`)
+
+numberOfAttempts = 7
+console.log(`le nombre de tentatives réalisées est de ${numberOfAttempts}`)
+
+
+// Troisième User Story
+const randomNumber = 6
+const solutionSentence = `la solution est ${randomNumber}`
+console.log(solutionSentence)
+
