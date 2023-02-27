a3f951b6c9705c46b9b34ea670f911a7847ffa8f
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-02-27 17:51:46 +0100

    Removing solution

--- app.js
+++ app.js
@@ -2,27 +2,12 @@ const maximumNumberOfTries = 10
 console.log(`le nombre maximum d'essais est de ${maximumNumberOfTries}`)
 
 
-function displayNumberOfAttempts(numberOfAttempts) {
-    console.log(`le nombre de tentatives réalisées est de ${numberOfAttempts}`)
-}
-
-
-function generateRandomNumber() {
-    const randomNumber = 6
-    return randomNumber
-}
-
-
 function main() {
     console.log("Bienvenue au jeu de devinette des nombres !")
 
     // Première User Story
-    const randomNumber = generateRandomNumber()
-    console.log(`la solution est ${randomNumber}`)
 
     // Deuxième User Story
-    let numberOfAttempts = 2
-    displayNumberOfAttempts(numberOfAttempts)
 }
 
 main()
