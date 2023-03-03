4ecba4439dabd7b8345b94a298fe06490ff2bd5e
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-03-03 12:08:32 +0100

    Adding Math example

---
+++ math.js
@@ -0,0 +1,15 @@
+// // J'initialise une constante contenant la valeur de PI
+// const pi = Math.PI
+// console.log(`La valeur de PI est ${pi}`)
+
+// // J'initialise une constante contenant la valeur de racine carrée de 2
+// const squareRootOfTwo = Math.SQRT2
+// console.log(`La valeur de la racine carrée de 2 est ${squareRootOfTwo}`)
+
+// Je génère un nombre compris entre 0 et 1
+const randomNumber = Math.random()
+console.log(`La valeur de "randomNumber" est ${randomNumber}`)
+
+// Je génère l'arrondi de 13,4
+const roundedNumber = Math.round(13.8)
+console.log(`L'arrondi de "roundedNumber" est ${roundedNumber}`)
