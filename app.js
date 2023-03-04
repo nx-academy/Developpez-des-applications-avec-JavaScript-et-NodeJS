5b4436135ce98c34ceb4ba82fbfb0448868e5fba
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-03-04 09:56:23 +0100

    Removing the solutions

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-const readline = require('readline')
-
-const inquierer = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
 const maximumNumberOfTries = 10
-// console.log(`le nombre maximum d'essais est de ${maximumNumberOfTries}`)
+console.log(`le nombre maximum d'essais est de ${maximumNumberOfTries}`)
 
 
 function displayNumberOfAttempts(numberOfAttempts) {
-    // console.log(`le nombre de tentatives réalisées est de ${numberOfAttempts}`)
+    console.log(`le nombre de tentatives réalisées est de ${numberOfAttempts}`)
 }
 
 
 function generateRandomNumber() {
-    const randomNumber = Math.floor(Math.random() * 10) + 1
+    const randomNumber = 6
     return randomNumber
 }
 
@@ -26,27 +19,23 @@ function main() {
     const randomNumber = generateRandomNumber()
     console.log(`la solution est ${randomNumber}`)
 
-    inquierer.question("Veuillez choisir un nombre entre 1 et 10 : ", (answer) => {
-        console.log(`vous avez choisi le nombre ${answer}`)
-        const guessNumber = Number(answer)
+    // la constante guessNumber qui va vous servir à valider vos conditions
+    const guessNumber = "thomas"
 
-        // Ajouter ici vos conditions
-        if (guessNumber < randomNumber) {
-            console.log("Too low")
-        } else if (guessNumber > randomNumber) {
-            console.log("Too high")
-        } else if (guessNumber === randomNumber) {
-            console.log("Well done")
-        } else {
-            console.log("Invalid input")
-        }
-    })
-
-    
+    // Ajouter ici vos conditions
+    if (guessNumber < randomNumber) {
+        console.log("Too low")
+    } else if (guessNumber > randomNumber) {
+        console.log("Too high")
+    } else if (guessNumber === randomNumber) {
+        console.log("Well done")
+    } else {
+        console.log("Invalid input")
+    }
 
 
-    // let numberOfAttempts = 2
-    // displayNumberOfAttempts(numberOfAttempts)
+    let numberOfAttempts = 2
+    displayNumberOfAttempts(numberOfAttempts)
 }
 
 main()
