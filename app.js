01b6ee726273921f00ed1b88739c5a9e393963a2
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-03-04 09:54:14 +0100

    Adding the solution

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+const readline = require('readline')
+
+const inquierer = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
 const maximumNumberOfTries = 10
-console.log(`le nombre maximum d'essais est de ${maximumNumberOfTries}`)
+// console.log(`le nombre maximum d'essais est de ${maximumNumberOfTries}`)
 
 
 function displayNumberOfAttempts(numberOfAttempts) {
-    console.log(`le nombre de tentatives réalisées est de ${numberOfAttempts}`)
+    // console.log(`le nombre de tentatives réalisées est de ${numberOfAttempts}`)
 }
 
 
@@ -19,23 +26,27 @@ function main() {
     const randomNumber = generateRandomNumber()
     console.log(`la solution est ${randomNumber}`)
 
-    // la constante guessNumber qui va vous servir à valider vos conditions
-    const guessNumber = "thomas"
+    inquierer.question("Veuillez choisir un nombre entre 1 et 10 : ", (answer) => {
+        console.log(`vous avez choisi le nombre ${answer}`)
+        const guessNumber = Number(answer)
 
-    // Ajouter ici vos conditions
-    if (guessNumber < randomNumber) {
-        console.log("Too low")
-    } else if (guessNumber > randomNumber) {
-        console.log("Too high")
-    } else if (guessNumber === randomNumber) {
-        console.log("Well done")
-    } else {
-        console.log("Invalid input")
-    }
+        // Ajouter ici vos conditions
+        if (guessNumber < randomNumber) {
+            console.log("Too low")
+        } else if (guessNumber > randomNumber) {
+            console.log("Too high")
+        } else if (guessNumber === randomNumber) {
+            console.log("Well done")
+        } else {
+            console.log("Invalid input")
+        }
+    })
+
+    
 
 
-    let numberOfAttempts = 2
-    displayNumberOfAttempts(numberOfAttempts)
+    // let numberOfAttempts = 2
+    // displayNumberOfAttempts(numberOfAttempts)
 }
 
 main()
