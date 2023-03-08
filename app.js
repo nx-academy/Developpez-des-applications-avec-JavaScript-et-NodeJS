bed6a2219f859870db90b977d260956df1406867
EmilieGDU <emilie.gendreau@yahoo.fr>
2023-03-08 13:20:03 +0100

    Update app.js
    Exercice terminé selon les instructions de l'issue #1

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-// Première User Story
-const maximumNumberOfTries = null
-console.log("Affichez votre solution ici")
+// PATTERN
+// const maximumNumberOfTries = null
+// console.log("Affichez votre solution ici")
 
+// Première User Story
+const maximumNumberOfTries = 10
+console.log("------------------------")
+console.log("----- User Story 1 -----")
+console.log("------------------------")
+console.log(`Le nombre maximum d'essai est de ${maximumNumberOfTries}`)
 
 // Deuxième User Story
-
+let numberOfAttempts = 0
+console.log("------------------------")
+console.log("----- User Story 2 -----")
+console.log("------------------------")
+console.log(`Le nombre de tentatives réalisées est de ${numberOfAttempts += 2}`)
+console.log(`Le nombre de tentatives réalisées est de ${numberOfAttempts += 1}`)
+console.log(`Le nombre de tentatives réalisées est de ${numberOfAttempts += 4}`)
 
 // Troisième User Story
+const randomNumber = 6
+const solutionMessage = `La solution est ${randomNumber}`
+console.log("------------------------")
+console.log("----- User Story 3 -----")
+console.log("------------------------")
+console.log(solutionMessage)
